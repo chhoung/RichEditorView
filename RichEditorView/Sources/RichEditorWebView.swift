0a36db87186e7ec8cea58a6a44587048714e2dbb
chhorng <phengchhorng@gmail.com>
2025-04-01 08:35:39 +1100

    wk webview - inject script for text wrapping

--- RichEditorView/Sources/RichEditorWebView.swift
+++ RichEditorView/Sources/RichEditorWebView.swift
@@ -14,5 +14,21 @@ public class RichEditorWebView: WKWebView {
     public override var inputAccessoryView: UIView? {
         return accessoryView
     }
+    
+    init(frame: CGRect) {
+        fatalError("init(frame:) has not been implemented")
+    }
 
+    public required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        
+        let textWrapScriptString = """
+                    var style = document.createElement('style');
+                    style.textContent = 'body { word-wrap: break-word; overflow-wrap: break-word; width: 100%; margin: 0; padding: 10px; } .editable { max-width: 100%; white-space: pre-wrap; }';
+                    document.getElementsByTagName('head')[0].appendChild(style);
+                """
+        
+        let textWrapScript = WKUserScript(source: textWrapScriptString, injectionTime: .atDocumentEnd, forMainFrameOnly: true)
+        self.configuration.userContentController.addUserScript(textWrapScript)
+    }
 }
